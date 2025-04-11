f3030b344118f935ffd6b4c8df78c69dec20035d
kkgomez2 <kkristoffergomez@gmail.com>
2025-04-11 11:37:54 -0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Search from "./components/Search";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search/>
+      </div>
+    </main>
+  )
+}
+
+export default App;
