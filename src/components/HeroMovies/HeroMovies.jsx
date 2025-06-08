76e67a00336073403cf77fca2ced77da08834251
kkgomez2 <kkristoffergomez@gmail.com>
2025-06-08 10:24:42 -0700

    Need to change to relative but this is what it should look like full width

--- src/components/HeroMovies/HeroMovies.jsx
+++ src/components/HeroMovies/HeroMovies.jsx
@@ -3,11 +3,39 @@ import "./HeroMovies.scss";
 const HeroMovies = ({ movieList }) => {
   const moviesToDisplay = movieList.slice(0, 3);
 
+  const firstMovie = movieList[0];
+  const secondMovie = movieList[1];
+  const thirdMovie = movieList[2];
+
   return (
     <div>
       <div className="hero-movies">
         <div className="hero-image-container">
-          {moviesToDisplay.map((movie, index) => (
+          <img
+            className="hero-image hero-image-main"
+            src={
+              firstMovie?.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${firstMovie.poster_path}`
+                : "/no-movie.png"
+            }
+          />
+          <img
+            className="hero-image hero-image-sub hero-image-left"
+            src={
+              secondMovie?.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${secondMovie.poster_path}`
+                : "/no-movie.png"
+            }
+          />
+          <img
+            className="hero-image hero-image-sub hero-image-right"
+            src={
+              thirdMovie?.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${thirdMovie.poster_path}`
+                : "/no-movie.png"
+            }
+          />
+          {/* {moviesToDisplay.map((movie, index) => (
             <>
               <img
                 className="hero-image"
@@ -19,7 +47,7 @@ const HeroMovies = ({ movieList }) => {
                 }
               />
             </>
-          ))}
+          ))} */}
         </div>
       </div>
     </div>
